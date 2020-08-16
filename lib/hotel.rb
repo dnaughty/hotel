00e7d832cc581361e7a64c3b2949df6dbc91ae1f
Denis Naughton <mjolinor75@hotmail.com>
2020-08-16 12:52:51 -0400

    Finshed hotel_project

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,5 +1,53 @@
 require_relative "room"
 
 class Hotel
+
+    attr_reader :rooms
+
+    def initialize(name,hash)
+        @name = name
+        @rooms = Hash.new
+        hash.each { |room_name,capacity| @rooms[room_name] = Room.new(capacity) }
+    end
+
+    def name
+        @name.split(" ").map { |x| x.capitalize }.join(" ")
+    end
+
+    def room_exists?(name)
+        return true if @rooms.include?(name)
+        return false
+    end
+
+    def check_in(person, room_name)
+        if !self.room_exists?(room_name)
+          puts "sorry, room does not exist"
+          return
+        end
+
+       #self.room_exists?(room_name) && self.add_occupant(room_name)
+
+       success = @rooms[room_name].add_occupant(person)
+    if success
+      puts "check in successful"
+    else
+      puts "sorry, room is full"
+    end
+  
+
+    end
+
+    def has_vacancy?
+        @rooms.values.any? { |room| !room.full? }
+      end
+
+    def list_rooms
+
+        @rooms.each do |name,room|
+            puts "#{name} : #{room.available_space}" 
+        end
+
+    end
+
   
 end
